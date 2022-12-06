2a9ec6625dcdb82eedc6603be8dbc7bc80ba20e4
Jin Jeanett Bakketun <jinjb@stud.ntnu.no>
2022-12-06 10:21:53 +0100

    timer and reaction time + score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class GameManager : MonoBehaviour
 {
@@ -30,59 +34,87 @@ public class GameManager : MonoBehaviour
     public SceneLoader _loader;
 
 
+    // Text in the UI.
+    [SerializeField] public TMP_Text scoreText, countdownText;
+
+    // Reaction time in milliseconds. CountdownTime in seconds. 
+    public float reactionTime, countdownTime;
+
+    // the score 
+    public int _score;
+
+
     void Start()
-    {   
+    {
+        countdownTime = 30;
+        DisplayTime(countdownTime);
         Spawn();
     }
 
     void Update()
     {
-        if (_inputs != null)
+        // Checks if the timer is still going.
+        if (countdownTime > 0)
         {
-            for (int i = 0; i < _inputs.Count; i++)
-            {   
-                if (_inputs[i].IsMatched()) // Constantly checks if a "match" happened
+            countdownTime -= Time.deltaTime;  // Subtracts the time from the timer set.  
+            DisplayTime(countdownTime);
+            reactionTime += (Time.deltaTime * 1000); // Adds the time between startime and now. 
+
+            if (_inputs != null)
+            {
+                for (int i = 0; i < _inputs.Count; i++)
                 {
-                    _inputs[i].SetMatched(false);
-                    for (int j = 0; j < _indices.Count; j++)
+                    if (_inputs[i].IsMatched()) // Constantly checks if a "match" happened
                     {
-                        Destroy(_inputs[j].gameObject); // deletes the input dragged to the animal in middle
-                    }   
-                    Destroy(_target.gameObject); // deletes animal in middle 
+                        _inputs[i].SetMatched(false);
+                        for (int j = 0; j < _indices.Count; j++)
+                        {
+                            Destroy(_inputs[j].gameObject); // deletes the input dragged to the animal in middle
+                        }
+                        Destroy(_target.gameObject); // deletes animal in middle 
 
-                    var background = Instantiate(_circlePrefab, _targetParent.position, Quaternion.identity); // adds circle background to animal 
-                    background.GetComponent<SpriteRenderer>().sortingOrder = 1;
+                        var background = Instantiate(_circlePrefab, _targetParent.position, Quaternion.identity); // adds circle background to animal 
+                        background.GetComponent<SpriteRenderer>().sortingOrder = 1;
 
-                    var solution = Instantiate(_targetPrefabs[_randomNumSound], _targetParent.position, Quaternion.identity); // adds correct animal 
-                    solution.GetComponent<SpriteRenderer>().sortingOrder = 2;
+                        var solution = Instantiate(_targetPrefabs[_randomNumSound], _targetParent.position, Quaternion.identity); // adds correct animal 
+                        solution.GetComponent<SpriteRenderer>().sortingOrder = 2;
 
-                    solution.Increase();
+                        solution.Increase();
 
-                    Destroy(solution.gameObject, 1f);
-                    Destroy(background.gameObject, 1f);
+                        Destroy(solution.gameObject, 1f);
+                        Destroy(background.gameObject, 1f);
 
-                    // Switches scene if 5 animals were shown
-                    if (_counter == 5) 
-                    {
-                        // Using "Invoke" the function is executed after 1 second
-                        _loader.Invoke("LoadNextScene", 1f);
+                        CalculateScore();
+
+                        // Switches scene if 5 animals were shown
+                        if (_counter == 5)
+                        {
+                            // Using "Invoke" the function is executed after 1 second
+                            _loader.Invoke("LoadNextScene", 1f);
+                        }
+                        // Otherwise spawn animals again 
+                        else
+                        {
+                            Spawn();
+                        }
+
                     }
-                    // Otherwise spawn animals again 
-                    else
-                    {
-                        Spawn();
-                    }
-
                 }
             }
         }
-        
+        else
+        {
+            Debug.Log("Countdown is over.");
+            //switch to quit scene. 
+        }
     }
 
 
     // Spawns animals on the screen 
-    public void Spawn()    
+    public void Spawn()
     {
+        reactionTime = 0; // Sets the timer to zero again for every new view of animals. 
+
         _counter += 1; // increase counter 
         _indices = Enumerable.Range(0, _inputPrefabs.Count).ToList(); // List of integers from 0 to *num of animal prefabs*
         _inputs = new List<DDInput>(); // Init list to store input animals in to access them later 
@@ -115,7 +147,20 @@ public class GameManager : MonoBehaviour
                 input.Init(_target); // Here we remember what input and target match 
             }
         }
-
-        
     }
+
+    private void DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(countdownTime / 60);
+        float seconds = Mathf.FloorToInt(countdownTime % 60);
+        countdownText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    private void CalculateScore()
+    {
+        int score = Mathf.FloorToInt(Mathf.Round((reactionTime)));
+        _score = score;
+        scoreText.text = score.ToString();
+    }
+
 }
